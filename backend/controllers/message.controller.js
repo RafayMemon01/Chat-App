d8dbdd2be7068858ac8474a4c7e6800860014e01
RafayMemon01 <rafayrashid200611@gmail.com>
2025-03-28 06:50:08 +0500

    add sendMessage functionality to handle message sending with optional image upload

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,4 +1,6 @@
-import Message from "../models/message.model";
+import Message from "../models/message.model.js";
+import * as cloudinary from 'cloudinary';
+
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -30,6 +32,36 @@ export const getMessages =async(req, res)=>{
     } catch (error) {
         
         console.log("Error in getMessages: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const sendMessage = async(req, res)=>{
+    try {
+        const {text, image} = req.body
+        const {id:receiverId} = req.params
+        const senderId = req.user._id
+
+        let imageURL;
+
+        if(image){
+            const uploadedResponse = await cloudinary.uploader.upload(image)
+            imageURL = uploadedResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageURL
+        });
+
+        //Todo: realtime functionality Socket
+        res.status(201).json(newMessage)   
+
+    } catch (error) {
+
+        console.log("Error in sendMessage: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 }
