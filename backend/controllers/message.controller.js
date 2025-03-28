f63e396cd6b3e9d9ce754bfdfb258a8d9e76a334
RafayMemon01 <rafayrashid200611@gmail.com>
2025-03-28 06:42:27 +0500

    add get messages functionality

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,5 @@
+import Message from "../models/message.model";
+
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
@@ -11,3 +13,23 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages =async(req, res)=>{
+    try {
+        const {id:userToChatId} = req.params
+        const senderId = req.user._id
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: senderId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: senderId },
+              ],
+        
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+        
+        console.log("Error in getMessages: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
